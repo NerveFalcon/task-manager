0ee5e1d40ef64cd90e46fbdc60d2348a3b3b0282
NerveFalcon <suraevoleg@gmail.com>
2021-05-22 15:42:54 +0300

    adminController debug

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -4,14 +4,14 @@ class AdminController extends Controller
 	public function ActionUserList(int $page = 1)
 	{
 
-		$this->view->Generate('admin/userListView.php', $page);
+		$this->view->Generate('admin/userListView.php');
 		return true;
 	}
 	
 	public function ActionPositionList(int $page = 1)
 	{
 
-		$this->view->Generate('admin/userListView.php', $page);
+		$this->view->Generate('admin/userListView.php');
 		return true;
 	}
 }
