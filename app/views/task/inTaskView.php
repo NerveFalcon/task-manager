82e186141d261c608fcac5de7b1bb6e71d047099
NerveFalcon <suraevoleg@gmail.com>
2021-05-25 18:55:31 +0300

    part InTask

--- app/views/task/inTaskView.php
+++ app/views/task/inTaskView.php
@@ -1,16 +1,18 @@
 <section class="inTask task">
 	<div class="top flex-container-row">
-		<h1 class="flex-item flex-item-row-first"><?php echo $params['title'] ?></h1>
-		<span class="status flex-item flex-item-row-last"><?php echo $params['status'] ?></span>
+		<h1 class="flex-item flex-item-row-first font32	"><?php echo $params['title'] ?></h1>
+		<span class="flex-item flex-item-row-last"><?php echo $params['status'] ?></span>
 	</div>
 	<span><?php echo $params['from'] ?></span>
 	<div class="desc">
 		<p><?php echo $params['desc'] ?></p>
-		<div class="files">
-			<?php if (isset($params['attachment'])) foreach ($params['attachment'] as $file) : ?>
-				<a href="<?php echo $file['url'] ?>"><?php echo $file['title'] ?></a>
-			<?php endforeach; ?>
-		</div>
+		<?php if (isset($params['attachment'])) : ?>
+			<div class="files">
+				<?php foreach ($params['attachment'] as $file) : ?>
+					<a href="/static/files/<?php echo $file['url'] ?>" target="_blank"><?php echo $file['title'] ?></a>
+				<?php endforeach; ?>
+			</div>
+		<?php endif; ?>
 	</div>
 	<div class="comments">
 		<h3>Комментарии</h3>
