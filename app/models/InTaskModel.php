d9f9010534336bc65b06157f1b21c2ea2c213e1f
NerveFalcon <suraevoleg@gmail.com>
2021-05-28 20:36:19 +0300

    DB debug

--- app/models/InTaskModel.php
+++ app/models/InTaskModel.php
@@ -15,13 +15,13 @@ class InTaskModel extends TaskModel
     public function GetListInTasks(int $page)
     {
         $showTasks = 4;
-        $res = $this->db->query("SELECT tasks.*, status.name as status FROM (tasks INNER JOIN workers ON workers.id = $_SESSION[id] AND tasks.id = workers.id_task) INNER JOIN status ON status.id = tasks.id_status ORDER BY tasks.deadline LIMIT " . ($page - 1)*$showTasks . "," . $showTasks . "");
+        $res = $this->db->query("SELECT tasks.*, status.en as status FROM (tasks INNER JOIN workers ON workers.id_user = $_SESSION[id] AND tasks.id = workers.id_task) INNER JOIN status ON status.id = tasks.id_status ORDER BY tasks.deadline LIMIT " . ($page - 1)*$showTasks . "," . $showTasks . "");
 
         return $res->fetch_all(1);
     }
     public function GetCountInTasks()
     {
-        $res = $this->db->query("SELECT COUNT(tasks.id) as count FROM tasks INNER JOIN workers WHERE workers.id = $_SESSION[id] AND tasks.id = workers.id_task");
+        $res = $this->db->query("SELECT COUNT(*) as count FROM tasks INNER JOIN workers WHERE workers.id_user = $_SESSION[id] AND tasks.id = workers.id_task");
 
         return $res->fetch_all(1)[0]['count'];
         $input = array();
@@ -43,7 +43,7 @@ class InTaskModel extends TaskModel
     }
     // public function GetTask(int $page)
     // {
-    //     //$res = $this->db->query("SELECT tasks.*, status.name as status FROM (tasks INNER JOIN workers ON workers.id = $_SESSION[id] AND tasks.id = workers.id_task) INNER JOIN status ON status.id = tasks.id_status ORDER BY tasks.deadline LIMIT " . ($page - 1)*$showTasks . "," . $showTasks . "");
+    //     //$res = $this->db->query("SELECT tasks.*, status.en as status FROM (tasks INNER JOIN workers ON workers.id_user = $_SESSION[id] AND tasks.id = workers.id_task) INNER JOIN status ON status.id = tasks.id_status ORDER BY tasks.deadline LIMIT " . ($page - 1)*$showTasks . "," . $showTasks . "");
 
     //     return $res->fetch_all(1);
     // }
