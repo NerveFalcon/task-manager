7c444d1f92641cb36411f7ad3196fbac888a31f3
Никита <83859886+M1X-web@users.noreply.github.com>
2021-05-28 23:45:06 +0300

    хз, что-то сделал

--- app/models/OutTaskModel.php
+++ app/models/OutTaskModel.php
@@ -3,4 +3,12 @@ include "TaskModel.php";
 class OutTaskModel extends TaskModel
 {
 	
+    public function getCountTasks()
+    {
+        $this->ThisSqlConnect();
+
+        $result = $this->db->query("SELECT COUNT(*) as count FROM tasks INNER JOIN workers WHERE workers.id_user = $_SESSION[id] AND tasks.id = workers.id_task");
+
+        return $this-.Fetch($result);
+    }
 }
