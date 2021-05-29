e3a0018ddd519f816288faeef1dc33f2951278db
Ziv-creater <hrustoshka@gmail.com>
2021-05-29 10:57:56 +0300

    pages

--- app/models/InTaskModel.php
+++ app/models/InTaskModel.php
@@ -12,27 +12,32 @@ class InTaskModel extends TaskModel
      *
      * @return array Масив задач
      */
-    public function GetListInTasks(int $page)
+    public function GetListInTasks(int $page, $filter, $showTasks = 4)
     {
-        $showTasks = 4;
-        $res = $this->db->query("SELECT tasks.*, status.ru as statusRu, status.but_in as butIn, status.but_out as butOut FROM (tasks INNER JOIN workers ON workers.id_user = $_SESSION[id] AND tasks.id = workers.id_task) INNER JOIN status ON status.id = tasks.id_status ORDER BY tasks.deadline LIMIT " . ($page - 1)*$showTasks . "," . $showTasks . "");
+        $filter = "'" . str_replace('+', '\', \'', $filter) . "'";
+        $res = $this->db->query("SELECT tasks.*, status.ru as status, status.but_in as butIn, status.but_out as butOut FROM (tasks INNER JOIN workers ON workers.id_user = $_SESSION[id] AND tasks.id = workers.id_task) INNER JOIN status ON status.id = tasks.id_status AND status.en IN ($filter) ORDER BY tasks.deadline LIMIT " . ($page - 1)*$showTasks . "," . $showTasks . "");
+		var_dump($filter);
+		var_dump($res);
 
         return $this->Fetch($res);
     }
-    public function GetCountInTasks()
+    public function GetCountInTasks($filter)
     {
-        $res = $this->db->query("SELECT COUNT(*) as count FROM tasks INNER JOIN workers WHERE workers.id_user = $_SESSION[id] AND tasks.id = workers.id_task");
+        $filter = "'" . str_replace('+', '\', \'', $filter) . "'";
 
-        // return $res->fetch_all(1)[0]['count'];
+        $res = $this->db->query("SELECT COUNT(*) as count FROM (tasks INNER JOIN workers ON workers.id_user = $_SESSION[id] AND tasks.id = workers.id_task) INNER JOIN status ON tasks.id_status = status.id AND status.en IN ($filter)");
+
+        return $this->Fetch($res)[0]['count'];
         $input = array();
         for ($i=1; $i <= 20; $i++) { 
             // $input['id_from'] = $i;
             // $input['title'] = "tittle$i";
-            $input['text']  = str_repeat('texttext ', 100);
+            // $input['text']  = str_repeat('texttext ', 100);
             // $input['deadline'] = "2021-11-$i";
             // $input['file_way'] = "fileWay$i";
+            $input['id_status'] = rand(1, 5);
+
             $this->UpdateOne("tasks", $input, $i);
-            // $input[$i]['id_status'] = $i % 5 + 1;
         }
         // for ($i = 1; $i <= 200; $i++)
         // {
