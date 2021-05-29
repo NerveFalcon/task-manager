97d76c6e4714ff790cb72532b9f22b4cb9b4383f
Ziv-creater <hrustoshka@gmail.com>
2021-05-29 12:44:09 +0300

    chat conec?

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -7,28 +7,21 @@ class MainController extends Controller
 	}
 	public function ActionChat()
 	{
+		if(!empty($_POST)){
+			$this->model->AddMessage();
+			header("Location: /");
+			return true;
+		}
 		$messages = $this->model->GetMessages();
-		// for ($i = 0; $i < 5; $i++)
-		// {
-		// 	if ($i != 2){
-		// 		$messages[$i]['author'] = "author$i";
-		// 		$messages[$i]['text'] = str_repeat("text ", 50);
-		// 	}
-		// 	else{
-		// 		$messages[$i]['author'] = $_SESSION['user'];
-		// 		$messages[$i]['text'] = "asdasd:";
-		// 	}
-		// 	$messages[$i]['date'] = "date";
-			
-		// }
+
 		foreach ($messages as $key => $value) {
 			$fio = explode(' ', $value['fio']);
-			$messages[$key]['fio'] = $fio[0] . " " . $fio[1][0] . ". " . $fio[2][0] . ".";
+			$messages[$key]['fio'] = $fio[0] . " " . substr($fio[1], 0, 2) . ". " . substr($fio[2], 0, 2) . ".";
 		}
-
 		$this->view->Generate('main/mainView.php', $messages);
 		return true;
 	}
+
 
 	public function ActionTest()
 	{
--- app/models/MainModel.php
+++ app/models/MainModel.php
@@ -1,23 +1,19 @@
 <?php
-class MainModel extends Model 
+class MainModel extends Model
 {
     public function __construct() {
         $this->ThisSqlConnect();
     }
 
     public function GetMessages() {
-        $res = $this->db->query("SELECT chat.text, chat.date, users.fio FROM chat INNER JOIN users ON chat.id_user = users.id ORDER BY chat.date");
-
-        // $messages = array();
-		// for ($i = 1; $i <= 5; $i++)
-		// {
-        //     $messages['id_user'] = $i;
-        //     $messages['text'] = str_repeat("text ", 50);
-
-        //     echo $this->Insert("chat", $messages);
-		// }
-
+        $res = $this->db->query("SELECT chat.text, chat.date, users.fio FROM chat INNER JOIN users ON chat.id_user = users.id ORDER BY chat.date DESC");
 
         return $this->Fetch($res);
+    }
+
+    public function AddMessage() {
+        $post = $this->GetPost();
+        $post['id_user'] = $_SESSION['id'];
+        $this->Insert("chat", $post);
     }
 }
